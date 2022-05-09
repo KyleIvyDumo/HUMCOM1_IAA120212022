8baede2a44f3ab41a23a0dfa05daab30789b8c82
Kyle Ivy Dumo <98094934+KyleIvyDumo@users.noreply.github.com>
2022-05-09 22:38:14 +0800

    Add files via upload

---
+++ Exercise 15/javascript/Script15.js
@@ -0,0 +1,12 @@
+function displayAssignedValues(){
+    var name = document.getElementById("name").value = "Kyle Ivy Suyat Dumo";
+    var age = document.getElementById("age").value = "18";
+    var phone = document.getElementById("phone").value = "0966 188 8145";
+    var email = document.getElementById("email").value = "20215655@s.ubaguio.edu";
+    var program = document.getElementById("program").value = "Information Technology";
+    var id = document.getElementById("id").value = "20215655";
+    var address = document.getElementById("address").value = "Irisan, Baguio City";
+    var year = document.getElementById("year").value = "1st Year";
+    var section = document.getElementById("section").value = "IAA1"; 
+
+}
